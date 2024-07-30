abc1680fefdf1ebfe3802b2806e8f11c7d4d59e8
Allan Silva <allandasilva33@outlook.com>
2024-07-30 04:16:18 -0300

    Estilização do menu mobile e adição da interatividade com JS. Fim do projeto.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggle() {
+    const menu = document.getElementById('menu')
+    menu.classList.toggle('active')
+    const btn = document.getElementById('btn')
+    btn.classList.toggle('active')
+}
