499b9cfb52c3529d8a1003a1be1c472e5a2b7879
Allan Silva <allandasilva33@outlook.com>
2024-07-31 00:46:17 -0300

    Adição da tela de loading

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function loading() {
+    const loader = document.getElementById('preload').style.display = 'none'
+    const container = document.getElementById('container-base').style.display = 'block'
+}
+
 function toggle() {
     const menu = document.getElementById('menu')
     menu.classList.toggle('active')
